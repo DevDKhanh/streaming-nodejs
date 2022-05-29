cbef58225f2f31fb8592f7d49d398b7ca8b88fcd
duykhanh <khanhtd@m2w.vn>
2022-05-30 03:51:35 +0700

    update

---
+++ server/cron/thumbnails.js
@@ -0,0 +1,31 @@
+const CronJob = require('cron').CronJob,
+    axios = require('axios'),
+    helpers = require('../helpers/helpers'),
+    config = require('../config/default'),
+    port = config.rtmp_server.http.port,
+    domain = config.domain.ip;
+
+const job = new CronJob(
+    '*/5 * * * * *',
+    function () {
+        axios
+            .get(domain + port + '/api/streams')
+            .then((response) => {
+                let streams = response.data;
+                if (typeof (streams['live'] !== undefined)) {
+                    let live_streams = streams['live'];
+                    for (let stream in live_streams) {
+                        if (!live_streams.hasOwnProperty(stream)) continue;
+                        helpers.generateStreamThumbnail(stream);
+                    }
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    null,
+    true
+);
+
+module.exports = job;
